5849ada2856d6a871f89815c88d759060a65c7e5
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2026-01-31 14:42:02 -0300

    Add files via upload

---
+++ aula_3/calculadora.py
@@ -0,0 +1,16 @@
+print('Calculadora Python')
+
+n1 = float(input('= '))
+n2 = float(input('= '))      
+
+print(f'''
+Soma - {n1 +  n2}
+Substração -  {n1  - n2}
+Multiplicação -  {n1 * n2}
+Divisão  -  {n1 / n2}
+''')
+
+
+      
+
+      
